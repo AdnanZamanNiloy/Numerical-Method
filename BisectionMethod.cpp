5672fc7ae70996aa5e7c73d927ca623cc9536876
Adnan Zaman <156665143+AdnanZamanNiloy@users.noreply.github.com>
2024-04-17 08:01:41 +0600

    BisectionMethod.cpp

--- BisectionMethod.cpp
+++ BisectionMethod.cpp
@@ -1,66 +1,42 @@
 #include <bits/stdc++.h>
-#pragma GCC optimize("Ofast,fast-math,unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 using namespace std;
 
-#define ll long long
-#define ld long double
-#define fr first
-#define sc second
-#define mp make_pair
-#define pb push_back
-#define pp pop_back
-#define nn "\n"
-#define mod 1000000007
-#define N 200005
-#define PI 2 * acos(0.0)
-
-#define sz(x) (int)(x).size()
-#define all(v) v.begin(), v.end()
-#define srt(v) sort(v.begin(), v.end())
-#define rev(v) reverse(v.begin(), v.end())
-#define rall(v) v.rbegin(), v.rend()
-#define rsrt(v) sort(v.rbegin(), v.rend())
-#define grtsrt(v) sort(v.begin(), v.end(), greater<int>())
-#define mnv(v) *min_element(v.begin(), v.end())
-#define mxv(v) *max_element(v.begin(), v.end())
-#define dbg(x) cerr << (#x) << " = " << (x) << "\n"
-// #define cerr if (false) cerr
-#define EP 0.01
-
-ld f(ld x)
+double Function(double x)
 {
-    return x * x * x - 4 * x - 10;
+    return x * x - 4;
 }
-
-void bisection(ld a, ld b)
+double Error(double a, double b)
 {
-    if (f(a) * f(b) >= 0)
-    {
-        cout << "You have not valid a b" << nn;
-        return;
-    }
-    ld c = a;
-    while ((b - a) >= EP)
+    return abs((a - b) / a);
+}
+double Bisection(double a, double b, double maxError)
+{
+    double c = 0;
+    while (Error(a, b) > maxError)
     {
         c = (a + b) / 2;
-
-        if (f(c) == 0.0)
-            break;
-
-        else if (f(c) * f(a) < 0)
+        if (Function(c) == 0)
+        {
+            return c;
+        }
+        else if (Function(c) * Function(a) < 0)
+        {
             b = c;
+        }
         else
+        {
             a = c;
+        }
+        cout << "a: " << a << " b: " << b << " c: " << c << " Error: " << Error(a, b) << endl;
     }
-    cout << "The value of root: " << c;
-    cout << nn;
+    return c;
 }
 
 int main()
 {
-    ld a, b;
-    cin >> a >> b;
-    bisection(a, b);
+    double a = 0;
+    double b = 3;
+    double maxError = 0.00000001;
+    cout << "Root: " << Bisection(a, b, maxError) << endl;
     return 0;
 }
